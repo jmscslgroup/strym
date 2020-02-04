50bd0854155adaec12fa4083843d8b2f770d9641
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-02-04 13:26:45 -0700

    Added more details to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def readme():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+        return long_description
 
 setuptools.setup(
     name='canviz',
@@ -9,9 +11,18 @@ setuptools.setup(
     scripts=['canviz'],
     author="Rahul Bhadani",
     author_email="rahulbhadani@email.arizona.edu",
-    description="A real time CAN data logging and visualization tool to work with USB-CAN Interface."
+    description="A real time CAN data logging and visualization tool to work with USB-CAN Interface.",
+    long_description=readme(),
     url="https://github.com/jmscslgroup/canviz",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'cantools',
+        'libusb1',
+        'pyserial',
+        'bitstring'
+        ],
     classifiers=[
         "Programming Language :: Pythion:: 3",
         "Framework :: AsyncIO",
@@ -19,4 +30,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Visualization",
         "License:: OSI Approved:: MIT License",
         ],
+    keywords='candata, can, autonomous vehicle, ACC, adaptive cruise control, USB, Panda',
+    include_package_data=True,
+    zip_safe=False
         )
