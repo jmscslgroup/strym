618015f79be33259698ad59dfef597831c993b91
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-02-17 18:07:41 -0700

    Example updated for more options

--- examples/strym_impl.py
+++ examples/strym_impl.py
@@ -12,19 +12,31 @@ import subprocess, shlex
 from subprocess import call
 import psutil
 import glob
-import os
+import numpy as np
 
 db = './newToyotacode.dbc'
 
-Viz = strym(dbcfile = db)
+init_options = {"path": "./"}
 
+
+Viz = strym(dbcfile = db, **init_options)
+
+# What message type to visualize
 message_type_to_visualize = 'SPEED'
+# What message attribute of the above message type to visualize
 message_attribute_number_to_visualize = 1
 
+# Above messages will be visualized only when you pass visualize=True
 visualize = False
-options = {"log": "info" }
+
+# What message IDs to be logged into CSV File?, If you don't specify this, all messages will be logged.
+type_list = np.arange(384, 400)
+type_list = np.append(180, type_list)
+
+options = {"log": "info", "match": "exact", "typelist":type_list}
 Viz.isolog(visualize, message_type_to_visualize, message_attribute_number_to_visualize,  **options)
 
 signal.signal(signal.SIGINT, Viz.kill)
 
 print('Datafile saved is {}'.format(Viz.logfile))
+
