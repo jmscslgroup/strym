90d60531cf493bd5bd80294e89984582184bb16d
Matthew Nice <mnice@Matthews-MacBook-Pro-8.local>
2020-05-13 14:43:58 -0500

    Adding Kalman Filter function
    This is version 1 of this kalman filter. It has room for improvement in tuning noise, as well as possible adjustment of the frequency of state estimates i.e. maybe down the road we will want to ask for estimates at X hz.

--- strym/DBC_Read_Tools.py
+++ strym/DBC_Read_Tools.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Author : George Gunter, Matthew Nice - Vanderbilt University
-# Contact: gunter.gl@gmail.com, matthew.nice@vanderbilt.edu, rahulbhadani@email.arizona.edu
-# with some modifications by Rahul Bhadani
+# Author : Matthew Nice - Vanderbilt University, with some modifications by Rahul Bhadani
+# Contact: matthew.nice@vanderbilt.edu, rahulbhadani@email.arizona.edu
 
 # Initial Date: Feb 17, 2020
 # License: MIT License
@@ -327,3 +326,86 @@ def findObjectData(x):
         return 1
     else:
         return -1
+
+def KF_leadDist(df, plot=False, sd = False):
+    """This function is to give a filtered estimation of the state of the lead distance in an offline batch.
+
+    Dataframe input needs only two columns: the measurements of lead distance, and the times of the measurements.
+    Columns assumed titled 'Time', and 'Message'.
+
+    Setting plot to True will output plots. SD to True adds in the standard deviationon the plots."""
+
+    df=df.sort_values(by = 'Time').reset_index(drop=True) #make sure df is time sorted
+
+    #cleaning up erroneous high lead distance values:
+    for i in range(0,len(df.Message)):
+        if df.Message[i] > 240:
+            df = df.drop(i)
+
+    times = df.Time.values
+    measurements = df.Message.values
+
+    initial_state_mean = [measurements[0], #distance
+                          0] #relv
+
+    transition_matrix = [[1, 1],
+                         [0, 1]]
+
+    observation_matrix = [[1, 0]]
+
+    observation_covariance = [[1]] #R
+
+    transition_covariance = [[1,0],
+                            [0,1]] #Q
+
+
+    kf1 = KalmanFilter(transition_matrices = transition_matrix,
+                      observation_matrices = observation_matrix,
+                      initial_state_mean = initial_state_mean,
+                      transition_covariance =  transition_covariance,
+                      observation_covariance = observation_covariance)
+
+    (filtered_state_means, filtered_state_covariances) = kf1.filter(measurements)
+
+    #calculating the variance of the states
+    if sd == True:
+        D_variance = []
+        D_sd=[]
+        rV_sd=[]
+        for i in range(0,len(filtered_state_covariances)):
+            D_variance.append(filtered_state_covariances[i][0][0])
+            D_sd.append(3*np.sqrt(filtered_state_covariances[i][0][0]))
+            rV_sd.append(1*np.sqrt(filtered_state_covariances[i][1][1]))
+
+        D_1sd_minus = [i * -.33 for i in D_sd]
+        D_1sd_plus = [i * .33 for i in D_sd]
+        D_sd_minus=  [i * -1 for i in D_sd] #3 st dev minus
+        Rv1sd_minus = [i * -1 for i in rV_sd]
+    if plot == True:
+        pt.figure(1)
+
+        pt.plot(times,measurements,'ro', markersize = 2, label='Measurements')
+        pt.plot(times,filtered_state_means[:, 0], 'bo', markersize = 2,label='Estimated State')
+        if sd == True:
+            pt.plot(times,measurements-D_sd, 'g--')
+            pt.plot(times,measurements+D_sd, 'g--',label='3SD')
+            pt.title('Measurements, Mean State, and Confidence Interval')
+        else:
+            pt.title('Measurements, and Mean Estimated State')
+        pt.xlabel('Time (seconds)')
+        pt.ylabel('Lead Distance (meters)')
+        pt.legend()
+        pt.show()
+
+        slope = pd.Series(np.gradient(df.Message), df.Time, name='CAN RelV')
+
+        pt.figure(2)
+        pt.plot(times,filtered_state_means[:, 1], 'b--',label='Estimated Relv State')
+        pt.plot(slope,'g--',marker='.',markersize = 5,ls='',label='Gradient from Measurements') #CAN data relv
+        pt.title('Relative Velocity State Estimation')
+        pt.xlabel('Time (seconds)')
+        pt.ylabel('Relative Velocity (meters/second)')
+        pt.legend()
+        pt.show()
+
+    return filtered_state_means, filtered_state_covariances
