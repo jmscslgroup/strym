917f1552a1d10f4f516b68922eeec3a93cd5f19b
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-07-25 15:34:31 -0700

    Bumped version to release 0.1.12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,9 +65,9 @@ author = 'Rahul Bhadani'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.1.10'
+version = u'0.1.12'
 # The full version, including alpha/beta/rc tags.
-release = u'0.1.10'
+release = u'0.1.12'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setuptools.setup(
     name='strym',
-    version='0.1.11',
+    version='0.1.12',
     author="Rahul Bhadani",
     author_email="rahulbhadani@email.arizona.edu",
     description="A real time CAN data logging and visualization tool to work with USB-CAN Interface.",
